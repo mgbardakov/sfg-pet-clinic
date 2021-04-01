b13d60c1512e2fee3773e2f852821deaccee15f6
mgbardakov <mgbardakov@gmail.com>
2021-04-01 20:29:09 +0300

    links fixed #44

--- pet-clinic-web/src/main/java/ru/tehnotron/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/tehnotron/sfgpetclinic/controllers/OwnerController.java
@@ -20,4 +20,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    String findOwners() {
+        return "notimplemented";
+    }
 }
