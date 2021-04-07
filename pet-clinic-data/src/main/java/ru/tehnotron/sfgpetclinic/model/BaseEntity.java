6dd4d9f6072282284894d02c081f286d183ec615
mgbardakov <mgbardakov@gmail.com>
2021-04-07 13:18:13 +0300

    converting base entity to JPA. closes #34

--- pet-clinic-data/src/main/java/ru/tehnotron/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/ru/tehnotron/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,15 @@
 package ru.tehnotron.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-public class BaseEntity<ID> implements Serializable {
-
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ID id;
 
     public ID getId() {
